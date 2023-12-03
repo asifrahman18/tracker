d77acf981b2ce0931aa5ae0f3766c05e546bfb63
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2023-12-03 22:36:16 +0600

    navBar

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -1,34 +1,39 @@
-'use client';
-import Link from 'next/link'
-import React from 'react'
-import { usePathname} from 'next/navigation'
+"use client";
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
 import { GiProgression } from "react-icons/gi";
-import classNames from 'classnames';
-
+import classNames from "classnames";
+import { Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
-
   const links = [
-    {label: 'Home', href: '/'},
-    {label: 'Tasks', href: '/tracker'},
-  ]
-    const currentPath = usePathname();
+    { label: "Home", href: "/" },
+    { label: "Tasks", href: "/tracker" },
+  ];
+  const currentPath = usePathname();
   return (
-    <nav className='flex space-x-6 border-b mb-6 px-5 h-14 items-center justify-center bg-cyan-500'>
-        <Link href='/'><GiProgression size={30} style={{color: 'white'}}/></Link>
-        <ul className='flex space-x-6 text-xl items-center'>
-          {links.map((link, i) => (
-            <li key={i}>
-              <Link className={classNames({
-                'text-white': link.href !== currentPath,
-                'text-black': link.href=== currentPath,
-                'hover:text-white transition-colors': true,
-              })} href={link.href}>{link.label}</Link>
-            </li>
-          ))}
-        </ul>
+    <nav className="border-b mb-6 px-5 h-14 bg-cyan-500">
+      <Container>
+        <Flex align="center" gap="2">
+          <Flex content="justify">
+            <Link href="/">
+              <GiProgression size={30} style={{ color: "white" }} />
+            </Link>
+            <ul className="flex space-x-6 text-xl items-center">
+              {links.map((link, i) => (
+                <li key={i}>
+                  <Link className="text-white" href={link.href}>
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+        </Flex>
+      </Container>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
